8c1652db95175511b97eaf6fd78bf8caa1169f2f
Stanley Lin <stanxlin@gmail.com>
2024-09-27 00:07:19 -0400

    Password encryption working and login progressed

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ const cors = require('cors')
 const mongoose = require("mongoose")
 const User = require('./models/User')
 const app = express();
+const bcrypt = require('bcryptjs')
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors())
 app.use(express.json())
@@ -11,11 +14,23 @@ mongoose.connect('mongodb+srv://stanxlin:mJJVrAkWPZGmEzWS@blog.0n1rb.mongodb.net
 
 app.post('/register', async (req,res) =>{
     const {username, password} = req.body;
-    const userDoc = await User.create({username, password})
-    res.json(userDoc)
+    try{
+        const userDoc = await User.create({
+            username, 
+            password:bcrypt.hashSync(password, salt)
+        })
+        res.json(userDoc)
+    } catch(e) {
+        res.status(400).json(e)
+    }
 
 })
 
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username})
+    const passOk = bcrypt.compareSync(password, userDoc.password)
+    res.json(passOk)
+})
+
 app.listen(4000);
-//mongodb+srv://<db_username>:<PCYlMaeV9eAfW3in>@cluster0.itjwf.mongodb.net/
-//PCYlMaeV9eAfW3in
